95740de6a54a178ac29e5174dd133f45eaeccf5e
Mark Allen <mrallen1@yahoo.com>
2012-09-30 22:38:41 -0500

    Add summary for 2012-09-17

--- bin/to_html.pl
+++ bin/to_html.pl
@@ -5,13 +5,13 @@ use File::Slurp qw(read_file write_file);
 use Text::Markdown qw(markdown);
 
 foreach my $md_fn ( @ARGV ) {
-    my $text = read_file( $md_fn, { binmode => ":utf8" } ) 
+    my $text = read_file( $md_fn, { binmode => ':utf8' } ) 
         or die "Couldn't read $md_fn: $!\n";
 
     my $html = markdown($text);
 
     my $html_fn = $md_fn =~ s/\.md/\.html/r;
-    write_file( $html_fn, { binmode => 'utf8' }, \$html ) 
+    write_file( $html_fn, { binmode => ':utf8' }, \$html ) 
         or die "Couldn't write $html_fn: $!\n";
 }
 
